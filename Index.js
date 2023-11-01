363825cae4644ccbf6f6ecd9f6577ff5b9dfca38
juanfepi27 <juanfepi27@gmail.com>
2023-11-01 16:47:24 -0500

    setting views path

--- Index.js
+++ Index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Routes = require('./Routes');
+const path =  require('path');
 
 class Index{
     static main(){
         const app = express();
         const port = 80;
         
+        app.set("views", path.join(__dirname, "Views"));
         app.set('view engine' , 'ejs');
         
         app.use('/' , Routes.main());
